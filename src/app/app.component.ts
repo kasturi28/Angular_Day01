520eb4c2f8f3cfb9b436ab936677114248d0c2d9
kasturi28 <kasturipansare2818@gmail.com>
2022-11-03 21:35:31 +0530

    Added_display bridgelabz website after clicking on the logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = "Hello from Bridgelabz";
   imageUrl="../assets/Bridgelabz logo.jpg";
+  url="https://www.bridgelabz.com/";
+  ngOnInit(): void{
+    this.title = "Hello from Bridgelabz"
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
 }
